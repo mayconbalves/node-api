2c4570996640d4ae2aa4e47d00e884799a016467
mayconbalves <mayconmustaine@hotmail.com>
2018-11-16 23:56:52 -0200

    chore(server): create first product

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
 const app = express()
 
@@ -7,8 +8,18 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {
 	useNweUrlParser: true
 })
 
+requireDir('./src/models')
+
+const Product = mongoose.model('Product')
+
 app.get('/', (req, res) => {
-	res.send('Hello man')
+	Product.create({ 
+		title: 'react native',
+		description: 'Build Apps with react',
+		url: 'http://github.com/facebook/react-native'
+	})
+
+	return res.send('Hello man')
 })
 
 app.listen(3001)
