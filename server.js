7474ced92f28856220118030c77de89699276e0b
mayconbalves <mayconmustaine@hotmail.com>
2018-11-24 08:51:32 -0200

    refactor(server): remove logic for add products

--- server.js
+++ server.js
@@ -10,16 +10,7 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {
 
 requireDir('./src/models')
 
-const Product = mongoose.model('Product')
-
-app.get('/', (req, res) => {
-	Product.create({ 
-		title: 'react native',
-		description: 'Build Apps with react',
-		url: 'http://github.com/facebook/react-native'
-	})
-
-	return res.send('Hello man')
-})
+//Rotas
+app.use('/api', require('./src/routes'))
 
 app.listen(3001)
