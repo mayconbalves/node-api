96f9e975fe1471272b68162566a184e3c413dfaa
mayconbalves <mayconmustaine@hotmail.com>
2018-11-26 22:51:15 -0200

    feat(controller): create method for find product by id

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router()
 
 const Product = require('./controllers/ProductController')
 routes.get('/products', Product.index)
+routes.get('/products/:id', Product.show)
 routes.post('/products', Product.store)
 
 module.exports = routes
