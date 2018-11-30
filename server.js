564450ccbe8b80826019f4eae077c2b0e3ec81b5
mayconbalves <mayconmustaine@hotmail.com>
2018-11-29 22:13:32 -0200

    feat(server): make public api

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
+const cors = require('cors')
 
 // Iniciando App
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 mongoose.connect('mongodb://localhost:27017/nodeapi', {
 	useNweUrlParser: true
