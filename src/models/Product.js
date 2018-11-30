c8f21f1c30dba294f47c6360f5a633c10b728910
mayconbalves <mayconmustaine@hotmail.com>
2018-11-29 22:02:38 -0200

    feat(product): create paginate

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate')
 
 const ProductScheme = new mongoose.Schema({
   title: {
@@ -18,5 +19,7 @@ const ProductScheme = new mongoose.Schema({
     default: Date.now
   }
 })
+
+ProductScheme.plugin(mongoosePaginate)
 
 mongoose.model('Product', ProductScheme)
